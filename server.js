0a11f63c3a79972e2412eb116acb503e9ba3ba78
Padunk <abraham.agel@gmail.com>
2020-02-15 21:59:24 +0700

    remove testing temporarily

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 'use strict';
 
+require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
-const expect = require('chai').expect;
 const cors = require('cors');
 const helmet = require('helmet');
-const mongoose = require('mongoose');
 
 const apiRoutes = require('./routes/api.js');
-const fccTestingRoutes = require('./routes/fcctesting.js');
-const runner = require('./test-runner');
-const threadSchema = require('./schema/Schema');
 
 const app = express();
 
@@ -37,9 +33,6 @@ app.route('/').get(function(req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-//For FCC testing purposes
-fccTestingRoutes(app);
-
 //Routing for API
 apiRoutes(app);
 
@@ -55,18 +48,18 @@ app.use(function(req, res, next) {
 //Start our server and tests!
 app.listen(process.env.PORT || 3000, function() {
     console.log('Listening on port ' + process.env.PORT);
-    if (process.env.NODE_ENV === 'test') {
-        console.log('Running Tests...');
-        setTimeout(function() {
-            try {
-                runner.run();
-            } catch (e) {
-                var error = e;
-                console.log('Tests are not valid:');
-                console.log(error);
-            }
-        }, 1500);
-    }
+    // if (process.env.NODE_ENV === 'test') {
+    //     console.log('Running Tests...');
+    //     setTimeout(function() {
+    //         try {
+    //             runner.run();
+    //         } catch (e) {
+    //             var error = e;
+    //             console.log('Tests are not valid:');
+    //             console.log(error);
+    //         }
+    //     }, 1500);
+    // }
 });
 
 module.exports = app; //for testing
