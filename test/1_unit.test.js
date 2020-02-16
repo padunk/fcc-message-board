62d2de51e7cd781231d9a29a8e01788c745470a6
Padunk <abraham.agel@gmail.com>
2020-02-16 12:16:15 +0700

    add unit test

--- test/1_unit.test.js
+++ test/1_unit.test.js
@@ -1,4 +1,38 @@
-const chai = require('chai');
-const assert = chai.assert;
+require('dotenv').config();
+const assert = require('chai').assert;
 
-suite('Unit Tests', function() {});
+const ThreadHandler = require('../controllers/ThreadHandler');
+const ReplyHandler = require('../controllers/ReplyHandler');
+
+suite('Unit Tests', function() {
+    const th = new ThreadHandler();
+    const rh = new ReplyHandler();
+
+    test('ThreadHandler is a class', done => {
+        assert.isDefined(th, 'ThreadHandler is defined');
+        assert.isObject(th, 'ThreadHandler is a object');
+        done();
+    });
+
+    test('ThreadHandler class methods', done => {
+        assert.isFunction(th.postThread, 'postThread is a function');
+        assert.isFunction(th.getAllThread, 'getAllThread is a function');
+        assert.isFunction(th.reportThread, 'reportThread is a function');
+        assert.isFunction(th.deleteThread, 'deleteThread is a function');
+        done();
+    });
+
+    test('ReplyHandler is a function', done => {
+        assert.isDefined(rh, 'ReplyHandler is defined');
+        assert.isObject(rh, 'ReplyHandler is a object');
+        done();
+    });
+
+    test('ReplyHandler class methods', done => {
+        assert.isFunction(rh.postReply, 'postReply is a function');
+        assert.isFunction(rh.getAllReply, 'getAllReply is a function');
+        assert.isFunction(rh.reportReply, 'reportReply is a function');
+        assert.isFunction(rh.deleteReply, 'deleteReply is a function');
+        done();
+    });
+});
